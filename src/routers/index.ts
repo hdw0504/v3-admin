b370d85caca0597be542453ac1c9f519532b428c
hdw0504 <768685351@qq.com>
2022-11-19 00:33:15 +0800

    update router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { ElNotification } from 'element-plus'
+import { storeToRefs } from 'pinia'
+import { initDynamicRouter } from './modules/dynamicRouter'
 import { staticRouter } from '@/routers/modules/staticRouter'
 import NProgress from '@/config/nprogress'
+import { LOGIN_URL } from '@/config/config'
+import { GlobalStore } from '@/stores'
+import { AxiosCanceler } from '@/api/helper'
+import { AuthStore } from '@/stores/modules/auth'
+
+const axiosCanceler = new AxiosCanceler()
 
 /**
  * @description 动态路由参数配置简介
@@ -34,24 +42,24 @@ router.beforeEach(async (to, from, next) => {
   // 1.NProgress 开始
   NProgress.start()
 
-  // // 2.在跳转路由之前，清除所有的请求
-  // axiosCanceler.removeAllPending()
+  // 2.在跳转路由之前，清除所有的请求
+  axiosCanceler.removeAllPending()
 
-  // // 3.如果是访问登陆页，直接放行
-  // if (to.path === LOGIN_URL)
-  //   return next()
+  // 3.如果是访问登陆页，直接放行
+  if (to.path === LOGIN_URL)
+    return next()
 
-  // // 4.判断是否有 Token，没有重定向到 login
-  // const globalStore = GlobalStore()
-  // if (!globalStore.token)
-  //   return next({ path: LOGIN_URL, replace: true })
+  // 4.判断是否有 Token，没有重定向到 login
+  const { token } = storeToRefs(GlobalStore())
+  if (!token)
+    return next({ path: LOGIN_URL, replace: true })
 
-  // // 5.如果没有菜单列表，就重新请求菜单列表并添加动态路由
-  // const authStore = AuthStore()
-  // if (!authStore.authMenuListGet.length) {
-  //   await initDynamicRouter()
-  //   return next({ ...to, replace: true })
-  // }
+  // 5.如果没有菜单列表，就重新请求菜单列表并添加动态路由
+  const { authMenuList } = storeToRefs(AuthStore())
+  if (!authMenuList.value.length) {
+    await initDynamicRouter()
+    return next({ ...to, replace: true })
+  }
 
   // 6.正常访问页面
   next()
---
+++ src/routers/modules/dynamicRouter.ts
@@ -0,0 +1,59 @@
+import { ElNotification } from 'element-plus'
+import { storeToRefs } from 'pinia'
+import router from '@/routers/index'
+import { getFlatArr } from '@/utils/util'
+import { LOGIN_URL } from '@/config/config'
+import { AuthStore } from '@/stores/modules/auth'
+import { notFoundRouter } from '@/routers/modules/staticRouter'
+
+// 引入 views 文件夹下所有 vue 文件
+const modules = import.meta.glob('@/views/**/*.vue')
+
+/**
+ * 初始化动态路由
+ */
+export const initDynamicRouter = async () => {
+  try {
+    // 1.获取菜单列表 && 按钮权限
+    const authStore = AuthStore()
+    const { authMenuList } = storeToRefs(authStore)
+    const { getAuthMenuList, getAuthButtonList } = authStore
+    await getAuthMenuList()
+    await getAuthButtonList()
+
+    // 2.判断当前用户有没有菜单权限
+    if (!authMenuList.value.length) {
+      ElNotification({
+        title: '无权访问',
+        message: '当前账号无任何菜单权限，请联系系统管理员！',
+        type: 'warning',
+        duration: 3000,
+      })
+      router.replace(LOGIN_URL)
+      // eslint-disable-next-line prefer-promise-reject-errors
+      return Promise.reject('No permission')
+    }
+
+    // 3.添加动态路由 (getFlatArr 方法把菜单全部处理成一维数组，方便添加动态路由)
+    const dynamicRouter = getFlatArr(JSON.parse(JSON.stringify(authMenuList.value)))
+    dynamicRouter.forEach((item: any) => {
+      if (item.children)
+        delete item.children
+      if (item.component)
+        item.component = modules[`/src/views${item.component}.vue`]
+
+      if (item.meta.isFull)
+        router.addRoute(item)
+      else
+        router.addRoute('layout', item)
+    })
+
+    // 4.最后添加 notFoundRouter
+    router.addRoute(notFoundRouter)
+  }
+  catch (error) {
+    // 💢 当按钮 || 菜单请求出错时，重定向到登陆页
+    router.replace(LOGIN_URL)
+    return Promise.reject(error)
+  }
+}
