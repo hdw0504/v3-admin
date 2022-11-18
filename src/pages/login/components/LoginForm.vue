56399cc9b8712bc983ad59add3aea5177a152a8f
hdw0504 <768685351@qq.com>
2022-11-19 00:33:50 +0800

    login page

--- src/pages/login/components/LoginForm.vue
+++ src/pages/login/components/LoginForm.vue
@@ -1,42 +1,41 @@
-<!-- <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { ElForm, ElNotification } from 'element-plus'
-import { CircleClose, UserFilled } from '@element-plus/icons-vue'
-// import md5 from 'js-md5'
+<script setup lang="ts">
+import type { FormInstance } from 'element-plus'
+import { ElNotification } from 'element-plus'
+import { storeToRefs } from 'pinia'
 import type { Login } from '@/api/interface'
 import { loginApi } from '@/api/modules/login'
 import { GlobalStore } from '@/stores'
 import { TabsStore } from '@/stores/modules/tabs'
-import { getTimeState } from '@/utils/util'
 import { HOME_URL } from '@/config/config'
+import { getTimeState } from '@/utils/util'
 import { initDynamicRouter } from '@/routers/modules/dynamicRouter'
 
 const router = useRouter()
 const tabsStore = TabsStore()
 const globalStore = GlobalStore()
 
-// 定义 formRef（校验规则）
-type FormInstance = InstanceType<typeof ElForm>
 const loginFormRef = ref<FormInstance>()
+const loading = ref(false)
+const loginForm = reactive<Login.ReqLoginForm>({ username: '', password: '' })
 const loginRules = reactive({
   username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
   password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
 })
+// resetForm
+const resetForm = () => {
+  loginFormRef.value?.resetFields()
+}
 
-const loading = ref(false)
-const loginForm = reactive<Login.ReqLoginForm>({ username: '', password: '' })
-const login = (formEl: FormInstance | undefined) => {
-  if (!formEl)
-    return
-  formEl.validate(async (valid) => {
+const login = () => {
+  loginFormRef.value?.validate(async (valid) => {
     if (!valid)
       return
     loading.value = true
     try {
       // 1.执行登录接口
-      const { data } = await loginApi({ ...loginForm, password: md5(loginForm.password) })
-      globalStore.setToken(data.access_token)
+      const { data } = await loginApi({ ...loginForm, password: loginForm.password })
+      const { token } = storeToRefs(globalStore)
+      token.value = data.access_token
 
       // 2.添加动态路由
       await initDynamicRouter()
@@ -48,7 +47,7 @@ const login = (formEl: FormInstance | undefined) => {
       router.push(HOME_URL)
       ElNotification({
         title: getTimeState(),
-        message: '欢迎登录 Geeker-Admin',
+        message: '欢迎登录',
         type: 'success',
         duration: 3000,
       })
@@ -58,58 +57,49 @@ const login = (formEl: FormInstance | undefined) => {
     }
   })
 }
-
-// resetForm
-const resetForm = (formEl: FormInstance | undefined) => {
-  if (!formEl)
-    return
-  formEl.resetFields()
-}
-
-onMounted(() => {
-  // 监听enter事件（调用登录）
-  document.onkeydown = (e: any) => {
-    e = window.event || e
-    if (e.code === 'Enter' || e.code === 'enter' || e.code === 'NumpadEnter') {
-      if (loading.value)
-        return
-      login(loginFormRef.value)
-    }
-  }
+// 监听回车键
+const { enter } = useMagicKeys()
+whenever(enter, () => {
+  login()
 })
 </script>
 
 <template>
-  <ElForm ref="loginFormRef" :model="loginForm" :rules="loginRules" size="large">
-    <el-form-item prop="username">
-      <el-input v-model="loginForm.username" placeholder="用户名：admin / user">
-        <template #prefix>
-          <el-icon class="el-input__icon">
-            <user />
-          </el-icon>
-        </template>
-      </el-input>
-    </el-form-item>
-    <el-form-item prop="password">
-      <el-input v-model="loginForm.password" type="password" placeholder="密码：123456" show-password autocomplete="new-password">
-        <template #prefix>
-          <el-icon class="el-input__icon">
-            <lock />
-          </el-icon>
-        </template>
-      </el-input>
-    </el-form-item>
-  </ElForm>
-  <div class="login-btn">
-    <el-button :icon="CircleClose" round size="large" @click="resetForm(loginFormRef)">
-      重置
-    </el-button>
-    <el-button :icon="UserFilled" round size="large" type="primary" :loading="loading" @click="login(loginFormRef)">
-      登录
-    </el-button>
+  <div v-bind="$attrs">
+    <ElForm ref="loginFormRef" :model="loginForm" :rules="loginRules" size="large">
+      <el-form-item prop="username">
+        <el-input v-model="loginForm.username" placeholder="用户名：admin / user">
+          <template #prefix>
+            <el-icon>
+              <user />
+            </el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="loginForm.password" type="password" placeholder="密码：123456" show-password autocomplete="new-password">
+          <template #prefix>
+            <el-icon>
+              <lock />
+            </el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-col :span="11">
+          <el-button w-full icon="CircleClose" round @click="resetForm()">
+            重置
+          </el-button>
+        </el-col>
+        <el-col :span="11" :offset="2">
+          <el-button w-full icon="UserFilled" round type="primary" :loading="loading" @click="login()">
+            登录
+          </el-button>
+        </el-col>
+      </el-form-item>
+    </ElForm>
   </div>
 </template>
 
 <style scoped lang="scss">
-@import "../index.scss";
-</style> -->
+</style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,29 +1,53 @@
 <script setup lang="ts" name="login">
-// import LoginForm from './components/LoginForm.vue'
-import SwitchDark from '@/components/SwitchDark/index.vue'
+import LoginForm from './components/LoginForm.vue'
+import { isDark } from '@/composables/dark'
+// import { storeToRefs } from 'pinia'
+// import { GlobalStore } from '@/stores'
+// const { assemblySize } = storeToRefs(GlobalStore())
 </script>
 
 <template>
-  <div flex="~ row center ">
-    <!-- <SwitchDark class="dark" /> -->
-    <img img-resize class="shadow-2xl rd-4" width="536px" height="354px" src="https://picsum.photos/536/354" alt="">
-    <!-- <div class="login-box">
-      <div class="login-left">
-        <img src="@/assets/images/login_left.png" alt="login">
-      </div>
-      <div class="login-form">
-        <div class="login-logo">
-          <img class="login-icon" src="@/assets/images/logo.svg" alt="">
-          <h2 class="logo-text">
-            Geeker-Admin
-          </h2>
-        </div>
-        <LoginForm />
-      </div>
-    </div> -->
+  <div class="bg">
+    <div class="box">
+      <el-switch
+        :model-value="isDark"
+        class="theme-switch" inline-prompt
+        active-color="#0a0a0a" inactive-color="#dcdfe6"
+        active-icon="Sunny" inactive-icon="Moon"
+        @change="toggleDark()"
+      />
+      <img img-resize w-536px h-354px shadow-2xl rd-4 src="https://picsum.photos/536/354" alt="">
+      <LoginForm class="form" flex-1 max-w-500px p-10 p-b-4 rd-4 shadow-xl />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-@import "./index.scss";
+.bg {
+  --at-apply: w-full h-full bg-cover flex items-center justify-center bg-gray-50;
+  background-image: url('@/assets/images/login_bg.svg')
+}
+
+.box {
+  --at-apply: w-90% h-90% relative flex flex-row justify-around items-center gap-10 p-10 m-10 rd-4;
+  background-color: hsla(0deg, 0%, 100%, 0.8);
+}
+
+.theme-switch {
+  --at-apply: absolute top-10 right-10
+}
+
+.dark {
+  .bg {
+    background-color: var(--el-fill-color-extra-light);
+  }
+  .box {
+    background-color: var(--el-mask-color);
+  }
+  .form{
+    --at-apply: shadow-dark;
+    background-color: var(--el-bg-color);
+
+  }
+}
 </style>
