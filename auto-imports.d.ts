3c469e0654bd95101e5bb5dfb134d0c38da5af30
hdw0504 <768685351@qq.com>
2022-11-20 21:48:02 +0800

    fix bug

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -9,8 +9,8 @@ declare global {
   const $shallowRef: typeof import('vue/macros')['$shallowRef']
   const $toRef: typeof import('vue/macros')['$toRef']
   const EffectScope: typeof import('vue')['EffectScope']
+  const ElLoading: typeof import('element-plus/es')['ElLoading']
   const ElMessage: typeof import('element-plus/es')['ElMessage']
-  const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
   const ElNotification: typeof import('element-plus/es')['ElNotification']
   const asyncComputed: typeof import('@vueuse/core')['asyncComputed']
   const autoResetRef: typeof import('@vueuse/core')['autoResetRef']
@@ -288,8 +288,8 @@ declare module 'vue' {
     readonly $shallowRef: UnwrapRef<typeof import('vue/macros')['$shallowRef']>
     readonly $toRef: UnwrapRef<typeof import('vue/macros')['$toRef']>
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
+    readonly ElLoading: UnwrapRef<typeof import('element-plus/es')['ElLoading']>
     readonly ElMessage: UnwrapRef<typeof import('element-plus/es')['ElMessage']>
-    readonly ElMessageBox: UnwrapRef<typeof import('element-plus/es')['ElMessageBox']>
     readonly ElNotification: UnwrapRef<typeof import('element-plus/es')['ElNotification']>
     readonly asyncComputed: UnwrapRef<typeof import('@vueuse/core')['asyncComputed']>
     readonly autoResetRef: UnwrapRef<typeof import('@vueuse/core')['autoResetRef']>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts" name="login">
 import LoginForm from './components/LoginForm.vue'
 import { isDark } from '@/composables/dark'
-
-// 解决 unplugin-auto-import 在加载登录页的时候并未注册 ElMessage 和 ElNotification
-if (!ElMessage || !ElNotification) {
-  ElMessage.closeAll()
-  ElNotification.closeAll()
-}
 </script>
 
 <template>
