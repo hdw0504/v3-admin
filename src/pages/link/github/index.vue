aeea24b60fa2d1f02428bd142063ba57109c1fe4
hdw0504 <768685351@qq.com>
2022-11-22 22:08:22 +0800

    update

---
+++ src/pages/link/github/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts" name="github">
+
+</script>
+
+<template>
+  <div text-center class="card-full">
+    <span text-2xl line-height-40>
+      GitHub 仓库：
+      <a underline class="color-[-webkit-link]" href="https://github.com/hdw0504/v3-admin" target="_blank">
+        https://github.com/hdw0504/v3-admin
+      </a>
+      🍒🍉🍊
+    </span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
