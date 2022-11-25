5f9b178536d9ebb90122da46a0b9bd9939862c98
hdw0504 <768685351@qq.com>
2022-11-25 23:53:19 +0800

    fix bug

--- src/composables/fullscreen.ts
+++ src/composables/fullscreen.ts
@@ -1,5 +1,6 @@
-// const useFullscreenFn = useFullscreen()
-// export const toggleFullscreen = useFullscreenFn.toggle
-// export const isFullscreen = useFullscreenFn.isFullscreen
+const useFullscreenFn = useFullscreen()
+export const toggleFullscreen = useFullscreenFn.toggle
+export const isFullscreen = useFullscreenFn.isFullscreen
 
-export const { toggle: toggleFullscreen, isFullscreen } = $(useFullscreen())
+// 下面写法走不了autoimport规则
+// export const { toggle: toggleFullscreen, isFullscreen } = $(useFullscreen())
